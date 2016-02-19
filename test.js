33f477ce2792ac8ca4c7b06042eb6c946ebd2fc7
lucky071118 <lucky071118@gmail.com>
2016-02-19 16:11:50 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,11 +1,21 @@
 $(document).ready(function(){
 	$(".btn").click(function(){
 		$(".btn").load("test.txt");
+		$.get("test.asp",function(data, status){
+			alert("Data: " + data + "\nStatus: " + status);
+		});
+		$.post("test.asp",{
+			name: "Donald Duck",
+			city: "Duckburg"
+			
+		},function(data,status){
+			alert("Data: " + data + "\nStatus: " + status);
+			
+		});
+       
 	});
-	$(".demo").fadeOut(500);
-	$.get("test.asp",function(data, status){
-            alert("Data: " + data + "\nStatus: " + status);
-        });
+	
+
 });
 
 
