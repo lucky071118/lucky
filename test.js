f840c71e638b2e67fc3d3b118a1b8dacbed5ba35
lucky071118 <lucky071118@gmail.com>
2016-02-19 15:47:16 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
 	$(".btn").click(function(){
-		$(".btn").load("test.txt");
+		$(".btn").load("test.txt",function(responseTxt, statusTxt, xhr){
+        if(statusTxt == "success")
+            alert("External content loaded successfully!");
+        if(statusTxt == "error")
+            alert("Error: " + xhr.status + ": " + xhr.statusText);
+    });
 	});
 	$(".demo").fadeOut(500);
 });
